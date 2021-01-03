87cfe4674c5ef00d9a57ca29f34099726d3db426
Eduardo <edusanto22@gmail.com>
2021-01-02 22:45:25 -0300

    Footer criado

---
+++ Frontend/src/pages/Global/Footer/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./styles.css";
+
+const Footer:React.FC = () => {
+    return(
+            <div className="container-footer">
+                
+                <footer>Eduardo de jesus. Todos os direitos reservados.</footer>
+
+            </div>
+
+    );   
+}
+
+export default Footer;
--- Frontend/src/routes.tsx
+++ Frontend/src/routes.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
 import NavBar from "./pages/Global/NavBar";
+import Footer from "./pages/Global/Footer";
 
 import Home from "./pages/Home/index";
 import Store from "./pages/Store/index";
@@ -12,6 +13,7 @@ const Routes:React.FC = () => {
             <NavBar/>
             <Route component={Home} path="/" exact/>
             <Route component={Store} path="/store" exact/>
+            <Footer/>
         </BrowserRouter>
 
     );
